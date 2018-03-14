01ea5f9b600fdc8229a0ed43aee7396d7d5ede8e
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-03-14 19:31:10 -0400

    Reg

--- src/app/user/components/registration/registration.component.ts
+++ src/app/user/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  onSubmit(registrationForm: NgForm) {
+    console.log('regForm',  registrationForm.value);
+    
+  }
+
 
 }
