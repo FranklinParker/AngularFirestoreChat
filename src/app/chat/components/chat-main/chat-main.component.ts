43a84f6883bffb6814afff11f93123c950bcf575
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-03-14 23:28:05 -0400

    Chat

--- src/app/chat/components/chat-main/chat-main.component.ts
+++ src/app/chat/components/chat-main/chat-main.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-main.component.css']
 })
 export class ChatMainComponent implements OnInit {
-
+  chatMessages: string = 'test \n new';
+  chatMessage: string;
   constructor() { }
 
   ngOnInit() {
+  }
+  onMessageKey(evt$) {
+    console.log(evt$);
+
   }
 
 }
