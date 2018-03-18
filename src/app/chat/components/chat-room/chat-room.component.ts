fb6c18f5ac56955c422045bfd773799562492564
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-03-17 20:15:24 -0400

    Test

--- src/app/chat/components/chat-room/chat-room.component.ts
+++ src/app/chat/components/chat-room/chat-room.component.ts
@@ -1,36 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import * as fromRoot from '../../../app.reducer';
 import {Store} from '@ngrx/store';
 import {ChatService} from '../../services/chat.service';
 import {Observable} from 'rxjs/Observable';
 import {ChatRoomModel} from '../../chat-room.model';
 import {MatSelectModule} from '@angular/material';
+import {Subscription} from 'rxjs/Subscription';
+import {UserModel} from '../../../user/user-model';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.css']
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnInit, OnDestroy {
   chatMessages = '';
   chatMessage: string;
   chatRooms$: Observable<ChatRoomModel[]>;
   selectedChatRoom: ChatRoomModel;
+  user: UserModel;
+  userSub: Subscription;
 
   constructor(private store: Store<fromRoot.State>) {
   }
 
   ngOnInit() {
     this.chatRooms$ = this.store.select(fromRoot.getChatRooms);
-    this.chatRooms$
-      .subscribe((chatRooms: ChatRoomModel[]) => {
-        console.log('chatRooms room:', chatRooms);
+    this.userSub = this.store.select(fromRoot.getUser)
+      .subscribe((user: UserModel) => {
+        this.user = user;
       });
 
   }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
   onMessageKey(evt$) {
     if (evt$.keyCode === 13) {
-      this.chatMessages += '\n' + this.chatMessage;
+      this.chatMessages += '\n' + this.user.name +
+        ': ' + this.chatMessage;
       this.chatMessage = '';
     }
 
